d69f308a5f8c1fd7e7d0bc32f601aed0ea2b2581
Pan Cebularz <Oliwier.zagorski@yahoo.com>
2022-10-31 12:20:58 +0100

    update

--- public/index.js
+++ public/index.js
@@ -4,8 +4,10 @@ import activeNavbarPage from './scripts/activeNavbarPage.js';
 import stickyNavbar from './scripts/stickyNavbar.js';
 import animatedCursor from './scripts/animatedCursor.js';
 import contactForm from './scripts/contactForm.js';
+import currentYear from './scripts/currentYear.js';
 
 window.addEventListener('scroll', stickyNavbar);
 window.addEventListener('load', animatedCursor);
 window.addEventListener('scroll', activeNavbarPage);
 window.addEventListener('load', contactForm);
+currentYear();
