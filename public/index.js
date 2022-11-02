b9c9200c760893ce14e3c1ba052cbc865e87bc74
Pan Cebularz <Oliwier.zagorski@yahoo.com>
2022-11-02 11:58:48 +0100

    animations update

--- public/index.js
+++ public/index.js
@@ -4,12 +4,12 @@ import activeNavbarPage from './scripts/activeNavbarPage.js';
 import stickyNavbar from './scripts/stickyNavbar.js';
 import animatedCursor from './scripts/animatedCursor.js';
 import contactForm from './scripts/contactForm.js';
-import currentYear from './scripts/currentYear.js';
 import scrollAnimations from './scripts/scrollAnimations.js';
+import animatedLetters from './scripts/animatedLetters.js';
 
 window.addEventListener('scroll', stickyNavbar);
 window.addEventListener('load', animatedCursor);
 window.addEventListener('scroll', activeNavbarPage);
 window.addEventListener('load', contactForm);
 window.addEventListener('scroll', scrollAnimations);
-currentYear();
+animatedLetters();
